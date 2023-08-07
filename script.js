4f14aab6a36eb677f5388acb4ba4b572beb0b7ba
navneet-chugh <navneetchugh15@gmail.com>
2023-08-07 22:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const changeButton = document.getElementById('change_button');
+  const resetButton = document.getElementById('reset_button');
+  
+  changeButton.addEventListener('click', () => {
+    const blockId = document.getElementById('block_id').value;
+    const color = document.getElementById('color_id').value;
+    const block = document.getElementById(blockId);
+
+    if (block) {
+      const gridItems = document.querySelectorAll('.grid-item');
+      gridItems.forEach(item => {
+        item.style.backgroundColor = 'transparent';
+      });
+
+      block.style.backgroundColor = color;
+    }
+  });
+
+  resetButton.addEventListener('click', () => {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => {
+      item.style.backgroundColor = 'transparent';
+    });
+  });
